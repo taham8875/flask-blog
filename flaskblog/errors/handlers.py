14a14a6aebda068e0b9f8edb97d90f91895b991e
Taha Ahmed <taha.m8875@gmail.com>
2022-12-18 15:04:34 +0200

    custom errors page

---
+++ flaskblog/errors/handlers.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+errors = Blueprint('errors', __name__)
+
+
+@errors.app_errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.jinja2'), 404
+
+
+@errors.app_errorhandler(403)
+def error_403(error):
+    return render_template('errors/403.jinja2'), 403
+
+
+@errors.app_errorhandler(500)
+def error_500(error):
+    return render_template('errors/500.jinja2'), 500
