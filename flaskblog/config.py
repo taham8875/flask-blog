8ca5012f0b03e011dce0540b3af39d57d0f8132a
Taha Ahmed <taha.m8875@gmail.com>
2022-12-18 17:33:07 +0200

    commit

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,11 +1,12 @@
 import os
+import myconfig
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = myconfig.SECRET_KEY
+    SQLALCHEMY_DATABASE_URI = myconfig.SQLALCHEMY_DATABASE_URI
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    MAIL_USERNAME = myconfig.MAIL_USERNAME
+    MAIL_PASSWORD = myconfig.MAIL_PASSWORD
